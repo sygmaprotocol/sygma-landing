e1d65d12bd3f11c270c8d7546a4e6e1e1ee8d4f0
sweetpea22 <cindychau24@me.com>
2022-06-15 10:53:38 -0400

    Add links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,13 +11,14 @@ export const Footer: React.FC = () => {
       <div className='links'>
         <div className='links--column'>
           <h4>Start Building</h4>
-          <a href="/" target="_blank" rel="noopener noreferrer">GitHub</a>
-          <a href="/" target="_blank" rel="noopener noreferrer">Docs</a>
+          <a href="https://github.com/ChainSafe/ChainBridge" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="https://chainbridge.chainsafe.io/" target="_blank" rel="noopener noreferrer">Docs</a>
         </div>
         <div className='links--column'>
           <h4>Community</h4>
-          <a href="/" target="_blank" rel="noopener noreferrer">Join Discord</a>
-          <a href="/" target="_blank" rel="noopener noreferrer">Blog</a>
+          <a href="https://discord.com/invite/xSAwrnCWcg" target="_blank" rel="noopener noreferrer">Join Discord</a>
+          <a href="https://blog.chainsafe.io/the-future-of-blockchain-bridges-3ae5cc9850b5" target="_blank" rel="noopener noreferrer">Blog</a>
+          <a href="https://chainsafe.io/" target="_blank" rel="noopener noreferrer">About ChainSafe</a>
         </div>
       </div>
       <div className='copyright'>
