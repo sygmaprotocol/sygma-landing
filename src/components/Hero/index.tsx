1d73eca465268dcf40cf255dff9f3f5790dd4cca
sweetpea22 <cindychau24@me.com>
2022-07-05 15:53:46 -0400

    Change copy

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -8,7 +8,7 @@ export const Hero: React.FC = () => {
       <div className="hero--header">
         <Header />
         <div className="title-wrapper">
-          <h1 className="title-text">Ground zero for building cross-chain applications.</h1>
+          <h1 className="title-text">Fast, cheap, and always on. Sygma is the practical choice for cross-chain communication.</h1>
           <div className='button-wrapper'>
             <a href="https://chainbridge.chainsafe.io/" rel="noopener noreferrer" target="_blank">Start Building</a>
             <a href="https://docs.google.com/forms/d/e/1FAIpQLSekXkenIvi4jCL1RdmxlNVg3_VkxI2nbFAK1EM4TIGE5b1hug/viewform" rel="noopener noreferrer" target="_blank">Join Pilot Program</a>
