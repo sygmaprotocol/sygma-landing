e5553df1b705fbb1f1f7db3f1081975c4f2ac49d
sweetpea22 <cindychau24@me.com>
2022-07-05 12:54:37 -0400

    Change footer

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -11,7 +11,7 @@ export const Hero: React.FC = () => {
           <h1 className="title-text">Ground zero for building cross-chain applications.</h1>
           <div className='button-wrapper'>
             <button><a href="/" rel="noopener noreferrer" target="_blank">Start Building</a></button>
-            <button><a href="/" rel="noopener noreferrer" target="_blank">Join Pilot Program</a></button>
+            <button><a href="https://docs.google.com/forms/d/e/1FAIpQLSekXkenIvi4jCL1RdmxlNVg3_VkxI2nbFAK1EM4TIGE5b1hug/viewform" rel="noopener noreferrer" target="_blank">Join Pilot Program</a></button>
           </div>
         </div>
       </div>
