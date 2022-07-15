12e99fc0ebe47b934476e05b50bf40b2d9a2e212
sweetpea22 <cindychau24@me.com>
2022-07-15 09:46:27 -0400

    Update copy

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -6,23 +6,27 @@ export const About: React.FC = () => {
     <article className='about'>
       <div className='single-column'>
         <h1>Sygma's multi-chain vision</h1>
-        <div className='text-wrapper'>
-        </div>
       </div>
-      <div className='single-column'>
-        <h2>UX so good that it goes unnoticed.</h2>
-        <div className='text-wrapper'>
-          <p>Sygma takes bridging blockchains to the next level by making bridges invisible to the user.</p>
-          <p>Users experience unified and consistent interfaces regardless of the underlying protocol. </p>
+      <section className='columns'>
+        <div className='column'>
+          <div className='column--content-wrapper'>
+            <h1>UX so good that it goes unnoticed.</h1>
+            <div className='text-wrapper'>
+              <p>Sygma takes bridging blockchains to the next level by making bridges invisible to the user.</p>
+              <p>Users experience unified and consistent interfaces regardless of the underlying protocol. </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className='single-column'>
-        <h2>Less manual effort over time.</h2>
-        <div className='text-wrapper'>
-          <p>Write your code once, use it in many places with Sygma’s portable business logic.</p>
-          <p>A community of developers are continuously adding integrations and tooling to decrease technical debt.</p>
+        <div className='column'>
+          <div className='column--content-wrapper'>
+          <h1>Less manual effort over time.</h1>
+            <div className='text-wrapper'>
+              <p>Write your code once, use it in many places with Sygma’s portable business logic.</p>
+              <p>A community of developers are continuously adding integrations and tooling to decrease technical debt.</p>
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
     </article>
   )
 }
