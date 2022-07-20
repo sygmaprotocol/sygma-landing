b598ef97cd7c7d0088a75b3da11f393b47f1e1f2
sweetpea22 <cindychau24@me.com>
2022-07-20 01:39:07 -0400

    Nit

--- src/components/Callout/index.tsx
+++ src/components/Callout/index.tsx
@@ -5,7 +5,7 @@ export const Callout: React.FC = () => {
   return (
     <article className='callout'>
       <div className='single-column'>
-        <h1>Built on the technology that has transferred <a className="callout-link" href="/" target="_blank" rel="noopener noreferrer">over $600 million in assets across top blockchains.</a>
+        <h1>Built on the technology that has transferred <a className="callout-link" href="https://github.com/ChainSafe/ChainBridge" target="_blank" rel="noopener noreferrer">over $600 million in assets across top blockchains.</a>
         </h1>
       </div>
     </article>
