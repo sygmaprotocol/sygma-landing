38b488b379cc2023e43b283cb0ddfa05b550e235
sweetpea22 <cindychau24@me.com>
2022-07-20 15:06:21 -0400

    Change link name

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -11,7 +11,7 @@ export const Hero: React.FC = () => {
           <h1 className="title-text">Going multi-chain is a growth multiplier.</h1>
           <p className="hero-description">Sygma is the interoperability layer for building cross-blockchain applications. </p>
           <div className='button-wrapper'>
-            <a href="/litepaper.pdf" rel="noopener noreferrer" target="_blank">Download Litepaper</a>
+            <a href="/sygma-litepaper.pdf" rel="noopener noreferrer" target="_blank">Download Litepaper</a>
             <a href="https://docs.google.com/forms/d/e/1FAIpQLSekXkenIvi4jCL1RdmxlNVg3_VkxI2nbFAK1EM4TIGE5b1hug/viewform" rel="noopener noreferrer" target="_blank">Join our Pilot</a>
           </div>
         </div>
