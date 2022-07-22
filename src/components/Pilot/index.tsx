ced8e12cc42c24721e29a77d1b0e5ccef3b3b80c
sweetpea22 <cindychau24@me.com>
2022-07-22 10:39:36 -0400

    update pilot  link

--- src/components/Pilot/index.tsx
+++ src/components/Pilot/index.tsx
@@ -7,7 +7,7 @@ export const Pilot: React.FC = () => {
       <div className='single-column'>
         <h1>Get early access and dedicated experts to help with integration.
         </h1>
-        <button><a href="https://docs.google.com/forms/d/e/1FAIpQLSekXkenIvi4jCL1RdmxlNVg3_VkxI2nbFAK1EM4TIGE5b1hug/viewform" target="_blank" rel="noopener noreferrer">Join Our Pilot</a></button>
+        <button><a href="https://share.hsforms.com/1xWV8j5P4RbyXoYppB5n4yw4kcdm" target="_blank" rel="noopener noreferrer">Join Our Pilot</a></button>
       </div>
     </article>
   )
