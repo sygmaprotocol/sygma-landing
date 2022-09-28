11ed69729980ad35ce003450dcccfae7e2f9dd5b
sweetpea22 <cindychau24@me.com>
2022-09-28 11:32:09 -0400

    Update route

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,50 +2,51 @@ import React, { useState } from 'react';
 import './index.scss';
 import { motion } from 'framer-motion';
 import './mobilenav.scss';
+import { Link } from 'react-router-dom';
 
 const sidebar = {
   open: (height = 2000) => ({
     clipPath: `circle(${height * 2 + 200}px at 3000px 400px)`,
     transition: {
-      type: "spring",
+      type: 'spring',
       stiffness: 20,
-      restDelta: 2
-    }
+      restDelta: 2,
+    },
   }),
   closed: {
-    clipPath: "circle(0px at 1200px 40px)",
+    clipPath: 'circle(0px at 1200px 40px)',
     transition: {
       delay: 0.25,
-      type: "spring",
+      type: 'spring',
       stiffness: 300,
       restDelta: 5,
       damping: 40,
-    }
+    },
   },
 };
 
 const variants = {
   open: {
-    transition: { staggerChildren: 0.14, delayChildren: 0.2 }
+    transition: { staggerChildren: 0.14, delayChildren: 0.2 },
   },
   closed: {
-    transition: { staggerChildren: 0.05, staggerDirection: -1 }
-  }
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
 };
 
 const staggerSelf = {
   open: {
     opacity: 1,
     transition: {
-      y: { stiffness: 40, velocity: 50}
-    }
+      y: { stiffness: 40, velocity: 50 },
+    },
   },
   closed: {
     opacity: 0,
     transition: {
-      y: { stiffness: 40, velocity: 50 }
-    }
-  }
+      y: { stiffness: 40, velocity: 50 },
+    },
+  },
 };
 
 export interface NavLink0 {
@@ -54,7 +55,6 @@ export interface NavLink0 {
   linkUrl: string;
   description?: string;
 }
-
 
 const navlinks: NavLink0[] = [
   {
@@ -66,11 +66,6 @@ const navlinks: NavLink0[] = [
     id: 1,
     name: 'Litepaper',
     linkUrl: '/sygma-litepaper.pdf',
-  },
-  {
-    id: 2,
-    name: 'About',
-    linkUrl: '/about',
   },
   {
     id: 5,
@@ -94,66 +89,86 @@ const navlinks: NavLink0[] = [
   },
 ];
 
-
 export const Header = () => {
-
   const [isOpen, toggleOpen] = useState(false);
-
 
   return (
     <nav className='nav'>
       <div className='brand'>
-        <img src="logo.png" alt="" />
+        <img src='logo.png' alt='' />
         <h4>Sygma</h4>
       </div>
       <motion.nav
         initial={false}
         className='mobile-menu'
-        animate={isOpen ? "open" : "closed"}
-      >
+        animate={isOpen ? 'open' : 'closed'}>
         <motion.div className='background' variants={sidebar} />
-          <motion.ul 
-            variants={variants}
-            className='navMenu'>
-          {navlinks.map(i => (
+        <motion.ul variants={variants} className='navMenu'>
+          <motion.li
+            className={'navlink'}
+            variants={staggerSelf}
+            whileHover={{ x: -8 }}
+            whileTap={{ scale: 0.93 }}>
+            <Link to='/about' className='noUnderline'>
+              About Sygma
+            </Link>
+          </motion.li>
+          {navlinks.map((i) => (
             <motion.li
               className={'navlink'}
               variants={staggerSelf}
-              whileHover={{ x: -8}}
-              whileTap={{ scale: 0.93 }}
-            >
-              <a target="__blank" rel="noopener noreferrer" href={i.linkUrl} className='noUnderline'>{i.name}</a>
-              </motion.li>
+              whileHover={{ x: -8 }}
+              whileTap={{ scale: 0.93 }}>
+              <a
+                target='__blank'
+                rel='noopener noreferrer'
+                href={i.linkUrl}
+                className='noUnderline'>
+                {i.name}
+              </a>
+            </motion.li>
           ))}
         </motion.ul>
         <button className='button' onClick={() => toggleOpen(!isOpen)}>
-          <svg width="26" height="26" viewBox="0 0 27 22" fill="none" strokeWidth={1.5}>
-            <motion.path strokeWidth={1.5} stroke="#000" d="M 2 2.5 L 24 2.5"
-            variants={{
-                closed: { d: "M 2 2.5 L 24 2.5" },
-                open: { d: "M 3 16.5 L 22 1" }
+          <svg
+            width='26'
+            height='26'
+            viewBox='0 0 27 22'
+            fill='none'
+            strokeWidth={1.5}>
+            <motion.path
+              strokeWidth={1.5}
+              stroke='#000'
+              d='M 2 2.5 L 24 2.5'
+              variants={{
+                closed: { d: 'M 2 2.5 L 24 2.5' },
+                open: { d: 'M 3 16.5 L 22 1' },
               }}
             />
             <motion.path
-              strokeWidth={1.5} stroke="#000" 
-              d="M 2 9.423 L 24 9.423"
+              strokeWidth={1.5}
+              stroke='#000'
+              d='M 2 9.423 L 24 9.423'
               variants={{
                 closed: { opacity: 1 },
-                open: { opacity: 0 }
+                open: { opacity: 0 },
               }}
               transition={{ duration: 0.1 }}
             />
-            <motion.path strokeWidth={1.5} stroke="#000" d="M 2 16.346 L 24 16.346"
+            <motion.path
+              strokeWidth={1.5}
+              stroke='#000'
+              d='M 2 16.346 L 24 16.346'
               variants={{
-                closed: { d: "M 2 16.346 L 24 16.346" },
-                open: { d: "M 3 1 L 22 16.346" }
+                closed: { d: 'M 2 16.346 L 24 16.346' },
+                open: { d: 'M 3 1 L 22 16.346' },
               }}
             />
           </svg>
         </button>
       </motion.nav>
     </nav>
-  )
-}
+  );
+};
 
 
