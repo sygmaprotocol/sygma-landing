120ae95b1b2f5c708277f9b3bdb2635edb7ef129
sweetpea22 <cindychau24@me.com>
2023-05-16 15:31:52 -0400

    migrate from cra to next

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { Router } from './pages/Router';
 import "./index.scss";
 import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './scrollToTop';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export const App: React.FC = () => (
-  <BrowserRouter>
-    <ScrollToTop />
-    <Router />
-  </BrowserRouter>
+  <ChakraProvider>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Router />
+    </BrowserRouter>
+  </ChakraProvider>
 );
