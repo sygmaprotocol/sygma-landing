351d6c18bb7d1f35145d7c2aa09f07f505a41a44
sweetpea22 <cindychau24@me.com>
2023-05-19 10:41:40 -0400

    Change config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
 }
 
 module.exports = {
+  output: 'export',
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
