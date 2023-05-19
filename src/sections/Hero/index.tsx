d1a1e8ff2b2015c0dea7eed8a223252ebba20442
sweetpea22 <cindychau24@me.com>
2023-05-19 10:34:24 -0400

    Change build folder

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -60,7 +60,7 @@ export default function Hero() {
           <div className='hidden lg:flex lg:flex-1 lg:justify-end'>
             <a
               href='#'
-              className='text-sm font-semibold leading-6 text-gray-900'>
+              className='text-sm font-semibold leading-6 text-primary-100'>
               Contact <span aria-hidden='true'>&rarr;</span>
             </a>
           </div>
