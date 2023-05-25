a7395b222ee6a8621afe197e1954f99108ca52fc
sweetpea22 <cindychau24@me.com>
2023-05-25 12:04:14 -0400

    Keep static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,16 +20,32 @@ import { HubspotProvider } from '@aaronhayes/react-use-hubspot-form';
 import type { Post } from '@/sections/BlogTeaser';
 
 // get posts from ghost blog
-export const getStaticProps: GetStaticProps<{
-  posts: Post;
-}> = async () => {
-  const res = await fetch(
-    `https://testing123.ghost.io/ghost/api/v3/content/posts/?key=${process.env.REACT_APP_GHOST_API_KEY}`
-  );
-  const parsedResponse = await res.json();
-  const posts = parsedResponse.posts;
-  return { props: { posts }, revalidate: 10 };
+export const getStaticProps = async (params: any) => {
+  const posts = await getPosts();
+  if (posts) {
+    const parsedPosts = JSON.parse(JSON.stringify(posts));
+    return {
+      props: {
+        posts: parsedPosts,
+      },
+    };
+  }
+  return {
+    props: {
+      posts: [],
+    },
+  };
 };
+
+const getPosts = async () => {
+  const response = await fetch(
+    `https://testing123.ghost.io/ghost/api/v3/content/posts/?key=${process.env.REACT_APP_GHOST_API_KEY}`
+  ).then((res) => res.json());
+  console.log(response.posts);
+
+  return response.posts;
+};
+
 
 export default function Home(props: any) {
   const { posts } = props;
