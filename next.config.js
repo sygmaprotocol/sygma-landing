444cedb4c32c751c2763ba9df373b646c8641400
sweetpea22 <cindychau24@me.com>
2023-06-26 15:32:07 -0400

    Fix env

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 
 const dotenv = require("dotenv");
 
-// const env = dotenv.parse(fs.readFileSync(".env"));
+const env = dotenv.parse(fs.readFileSync(".env"));
 
 const nextConfig = {
   reactStrictMode: true,
-  // env: env,
+  env: env,
 };
 
 module.exports = {
--- src/pages/solutions.tsx
+++ src/pages/solutions.tsx
@@ -1,7 +1,5 @@
 import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 import FormPageTemplate from "@/sections/FormPageTemplate";
-import SlidingWords from "@/sections/SlidingWords";
 import SolutionsHero from "@/sections/SolutionsHero";
 import SubscribeNewsletter from "@/sections/SubscribeNewsletter";
 import { HubspotProvider } from "@aaronhayes/react-use-hubspot-form";
@@ -9,15 +7,15 @@ import { HubspotProvider } from "@aaronhayes/react-use-hubspot-form";
 export default function solutions(props: any) {
   return (
     <HubspotProvider>
-      <SolutionsHero/>
+      <SolutionsHero />
       <FormPageTemplate
         hsPortalId="39701769"
         hsFormId="f0ee17fe-2445-49c5-be1e-32999a97563b"
         targetName="#solutions"
       />
-    
-      <SubscribeNewsletter/>
+
+      <SubscribeNewsletter />
       <Footer />
-      </HubspotProvider>
+    </HubspotProvider>
   );
 }
