ae012644870b7f6d03bfabd64d98aedaa9469a4c
sweetpea22 <cindychau24@me.com>
2023-06-26 15:37:44 -0400

    Fix env

--- src/ghost-client.ts
+++ src/ghost-client.ts
@@ -3,8 +3,8 @@ import GhostContentAPI, {PostsOrPages} from '@tryghost/content-api'
 // Create API instance with site credentials
 
 const api = new GhostContentAPI({
-  url: process.env.GHOST_URL as string,
-  key: process.env.GHOST_KEY as string,
+  url: process.env.ghostUrl as string,
+  key: process.env.ghostKey as string,
   version: "v5.0"
 });
 
