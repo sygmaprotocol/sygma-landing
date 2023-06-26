b22967ee3087e0cad1c3e9b740bede3c3a865ebe
sweetpea22 <cindychau24@me.com>
2023-06-26 16:38:22 -0400

    Add key to iterator

--- src/sections/BlogTeaser/index.tsx
+++ src/sections/BlogTeaser/index.tsx
@@ -34,7 +34,9 @@ const BlogTeaser: React.FC<{ posts: Post[] }> = (props) => {
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts
-            .map((post: Post) => <BlogCard {...post} showExcerpt={true} />)
+            .map((post: Post) => (
+              <BlogCard key={post.id} {...post} showExcerpt={true} />
+            ))
             .slice(0, 3)}
         </div>
       </div>
