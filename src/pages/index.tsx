02fdd4014d425a9c59af8283134838bdbaecad57
sweetpea22 <cindychau24@me.com>
2023-06-28 17:05:30 -0400

    Remove ghost client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,18 @@
 import Head from "next/head";
 import Hero from "@/sections/Hero";
 import Footer from "@/components/Footer";
-import { GetStaticProps } from "next";
 import CTA from "@/sections/SubscribeNewsletter";
 import LogoCloud from "@/components/LogoCloud";
 import BuildersProgram from "@/sections/BuildersProgram";
 import { HubspotProvider } from "@aaronhayes/react-use-hubspot-form";
 import SocialBanner from "@/sections/SocialBanner";
-import { getAllPosts } from "@/ghost-client";
 import DevFeature1 from "@/sections/DevFeature1";
 import DevFeature2 from "@/sections/DevFeature2";
 import DevJourney from "@/sections/DevJourney";
 import SlidingWords from "@/sections/SlidingWords";
 import CoreFeatures from "@/sections/CoreFeatures";
-import BlogTeaser from "@/sections/BlogTeaser";
-
-// export const getStaticProps: GetStaticProps = async () => {
-//   const posts = await getAllPosts();
-
-//   if (!posts) {
-//     return {
-//       notFound: true,
-//     };
-//   }
-
-//   return {
-//     props: { posts },
-//   };
-// };
 
 export default function Home(props: any) {
-  // const { posts } = props;
   return (
     <HubspotProvider>
       <>
